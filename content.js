e30b6d8af1726f3c17b8fdfde31e256344d57c6a
Lifeguards <emmettglaser@gmail.com>
2019-07-09 20:42:46 -0700

    payment shortcuts

--- content.js
+++ content.js
@@ -1,34 +1,88 @@
 "use strict";
 function paymentRoutine() {
+    let rightBlock = document.getElementById('pos_right_block');
     let itemLookup = document.getElementById('itemLookup');
-    const parent = itemLookup.parentElement;
+    let parent = itemLookup.parentElement;
     const reinput = () => {
         console.log('hek');
-        const quickInput = `
-		<input class="field ac_field" type="text" id="item_name" 
+        const quickInputText = `
+		<input class="field ac_field" type="text" id="item_name_auto" 
 		onfocus="shopItemFieldOnFocus(this, 'item_id');" value="" autocomplete="on"
 		onkeydown="if(event.keyCode === 13)
 		{
-			showResources($(POSMenu.formId),
+			setTimeout(() =>
+			{
+				showResources($(POSMenu.formId),
 				'https://ic.clubautomation.com/payment/right-side-view?add_item=1&amp;add_item_by_id=1&amp;' + $(this.form).serialize(),
 				'pos_right_block'
-			);
-			
-		}">
+				);
+			}, 100);	
+		}
+		POSMenu.isInitialised = false;
+		closeModalForm();
+		">
 		`;
-        //POSMenu.isInitialised = false;
-        //closeModalForm();
         itemLookup = document.getElementById('itemLookup');
+        parent = itemLookup.parentElement;
+        let greenButtons = document.getElementById('green_buttons');
+        /* start modifying stuff */
         mut.disconnect();
         if (itemLookup)
             itemLookup.remove();
-        parent.innerHTML += quickInput;
-        parent.children[1].focus();
-        mut.observe(parent, { childList: true });
+        parent.innerHTML += quickInputText;
+        let quickInput = parent.children[1];
+        let scanItem = document.getElementById('scanItem');
+        scanItem.innerHTML = 'Item Name:';
+        quickInput.focus();
+        let gl = greenButtons.children;
+        console.log(gl);
+        for (let i = 0; i < 2; ++i) {
+            gl[0].remove();
+        }
+        let paymentButton = gl[0];
+        mut.observe(rightBlock, { childList: true, subtree: true });
+        /* now we add the key shortcuts */
+        window.onkeydown = (event) => {
+            if (event.ctrlKey) {
+                switch (event.keyCode) {
+                    case 188: // comma key
+                        paymentButton.click();
+                        break;
+                    case 191: // slash key
+                        quickInput.focus();
+                        break;
+                }
+            }
+        };
     };
     const mut = new MutationObserver(reinput);
-    mut.observe(parent, { childList: true });
+    mut.observe(rightBlock, { childList: true, subtree: true });
     reinput();
+    const popMut = new MutationObserver(() => {
+        /* check if the take payment popup has come up */
+        let isPayment = document.getElementById('take_payment');
+        if (!isPayment)
+            return;
+        /* check if house charge is avaliable */
+        let isHouseCharge = document.getElementById('typeButton2');
+        if (!isHouseCharge) {
+            //probably put up something saying no house charge
+            return;
+        }
+        /* disconnect observer so we can change things */
+        popMut.disconnect();
+        /* inject script */
+        let sc = document.createElement('script');
+        sc.innerHTML = 'POSMenu.selectPaymentType(2, false)';
+        document.body.appendChild(sc);
+        sc.remove();
+        /* click pay button and we good */
+        let payButton = document.getElementById('payButton');
+        payButton.click();
+        /* re-observe */
+        //popMut.observe(document.body, {childList: true, subtree: true});
+    });
+    popMut.observe(document.body, { childList: true, subtree: true });
 }
 function checkinRoutine() {
     const CheckoutUrl = 'https://ic.clubautomation.com/payment?user_id=';
